5526255ffcae9a9f62905fade1e75c11dc1e9869
Ajith Ranka <ajith@mapbox.com>
2017-08-30 14:42:43 +0530

    UI mockup, thanks @tridip1931!

---
+++ mockup/script.js
@@ -0,0 +1,10 @@
+// Set personal token
+mapboxgl.accessToken = 'pk.eyJ1IjoidHJpZGlwMTkzMSIsImEiOiJjajVobTc1c3MxeXNyMnFucXV5cnVyOWhvIn0.xAsGvnYs57UMqlwdAQP5nA';
+
+// Create instance of GL map
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/dark-v9', // stylesheet location
+    center: [77.5943, 12.975], // starting position [lng, lat]
+    zoom: 12 // starting zoom
+});
